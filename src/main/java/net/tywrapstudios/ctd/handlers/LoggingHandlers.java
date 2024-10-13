c8e6f09e3cdbc17522eeaf72f63e06ebac4043ae
TywrapStudios <tywrap.studio@gmail.com>
2024-10-13 16:56:59 +0200

    1.3.2, LoggingHandlers are now compatible with Config reloads

--- src/main/java/net/tywrapstudios/ctd/handlers/LoggingHandlers.java
+++ src/main/java/net/tywrapstudios/ctd/handlers/LoggingHandlers.java
@@ -8,31 +8,34 @@ import org.slf4j.Logger;
 public class LoggingHandlers {
     static Logger logger = ChatToDiscord.LOGGER;
     static Logger debug = ChatToDiscord.DEBUG;
-    static Config config = Manager.getConfig();
 
     public static void info(String message) {
         logger.info(message);
     }
 
     public static void warn(String message) {
+        Config config = Manager.getConfig();
         if (!config.suppress_warns) {
             logger.warn(message);
         }
     }
 
     public static void error(String message) {
+        Config config = Manager.getConfig();
         if (!config.suppress_warns) {
             logger.error(message);
         }
     }
 
     public static void debug(String message) {
+        Config config = Manager.getConfig();
         if (config.debug_mode) {
             debug.info(message);
         }
     }
 
     public static void debugWarning(String message) {
+        Config config = Manager.getConfig();
         if (config.debug_mode&&!config.suppress_warns) {
             debug.warn(message);
         }
